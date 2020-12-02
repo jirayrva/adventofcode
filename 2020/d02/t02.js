548ad15c5e7c35e863083c081fbcaf1e4ae60a95
Jirayr Arzoumanian <jirayr@jva.am>
2020-12-02 09:47:37 +0100

    refactor: use R.xor

--- 2020/d02/t02.js
+++ 2020/d02/t02.js
@@ -8,10 +8,10 @@ const { arrayToObj } = require("./functions");
 const parseRow = R.compose(arrayToObj, R.match(/(\d+)-(\d+) (\w): (\w+)/));
 
 const isValidPassword = ({ password, policy }) =>
-  (password.charAt(policy.min - 1) === policy.ch &&
-    password.charAt(policy.max - 1) !== policy.ch) ||
-  (password.charAt(policy.min - 1) !== policy.ch &&
-    password.charAt(policy.max - 1) === policy.ch);
+  R.xor(
+    password.charAt(policy.min - 1) === policy.ch,
+    password.charAt(policy.max - 1) === policy.ch
+  );
 
 const app = R.compose(R.length, R.filter(isValidPassword), R.map(parseRow));
 
