c305de2b9e1aa731b6e004ede1fd143ed661c226
Jirayr Arzoumanian <jirayr@jva.am>
2020-12-02 01:47:27 +0100

    refactor: t01 to use more of R

--- 2020/d01/t01.js
+++ 2020/d01/t01.js
@@ -2,18 +2,14 @@
 
 const R = require("ramda");
 const input = require("./input");
-const { findMatch, log } = require("./functions");
+const { findMatch, log, logArray } = require("./functions");
 
 const findMatch2020 = findMatch(2020);
 
-const logN0N1 = ([n0, n1]) => (log(`n0: ${n0}, n1: ${n1}`), [n0, n1]);
-
-const multiplyN0N1 = ([n0, n1]) => R.multiply(n0, n1);
-
 // >>>> pure functions above this line
 
-const app = R.compose(log, multiplyN0N1, logN0N1, findMatch2020);
+const app = R.compose(R.product, logArray, findMatch2020);
 
 // >>>> setup above this line, only execution below here
 
-app(input);
+log(app(input));
